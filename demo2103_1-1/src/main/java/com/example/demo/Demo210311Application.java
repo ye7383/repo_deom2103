8e65d74b5ca9d5c7d6f4add9a0a176beae10bba9
ye7383 <kssd6008@naver.com>
2021-05-20 11:04:25 +0900

    2021.05.20 JDK11 리액트 부분은 미포함

---
+++ demo2103_1-1/src/main/java/com/example/demo/Demo210311Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo210311Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo210311Application.class, args);
+	}
+
+}
